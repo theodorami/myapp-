46461e5e61f6e982dd65b331387b01b97f6d3e21
theodorami <44063591+theodorami@users.noreply.github.com>
2019-12-03 19:57:28 +0100

    !

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hi Armin und Tino!');
+  res.send('Hi Armin und Tino');
 });
 
 app.listen(3000, function () {
