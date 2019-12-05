1491db649db76d1f9bd809632a24637bc0ae3e54
theodora miehlke <theodora.miehlke@gmx.de>
2019-12-05 15:20:48 +0100

    update package.json

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hi Armin und Tino!');
+  res.send('Hi Armin und Tino');
 });
 
-app.listen(3000, function () {
+
+
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!');
 });
